683a5ca3ab37869e768ed8294c144858fac9e4a1
Volodymyr Syromiatnykov <vladimir.fess@gmail.com>
2025-07-24 13:57:43 -0400

    feat: introduce ephemeral secret version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.3"
+  required_version = ">= 1.10"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.16"
+      version = ">= 5"
     }
   }
 }
